effd4f18060a1dc0866a56dcb30a89497ecf31be
Cole Johnson <coleharrisjohnson@gmail.com>
2020-04-26 14:12:28 -0700

    zoom using '[' and ']'

--- classes/pannable_camera.gd
+++ classes/pannable_camera.gd
@@ -3,12 +3,16 @@ class_name Pannablecamera
 
 
 func _process(delta):
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_key_pressed(KEY_RIGHT):
 		translate(Vector3(3, 0, 0))
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_key_pressed(KEY_LEFT):
 		translate(Vector3(-3, 0, 0))
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_key_pressed(KEY_UP):
 		translate(Vector3(0, 3, 0))
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_key_pressed(KEY_DOWN):
 		translate(Vector3(0, -3, 0))
+	if Input.is_key_pressed(KEY_BRACKETRIGHT):
+		translate(Vector3(0, 0, 3))
+	if Input.is_key_pressed(KEY_BRACKETLEFT):
+		translate(Vector3(0, 0, -3))
 	pass
